ad404d47f6f48488f1ed2eb71efbeb5f696247d8
Krystian, Kogut <308128@office.umcs.pl>
2024-03-03 20:35:13 +0100

    Initial project files commit

---
+++ src/test/java/project/medsys/MedsysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.medsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedsysApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
