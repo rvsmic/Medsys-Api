e9d50f9a13c2f6fd029294496f221e7dcfa1c2bf
Krystian, Kogut <308128@office.umcs.pl>
2024-03-15 22:49:15 +0100

    Adding azure hosting properties

--- src/main/java/com/medsys/medsysapi/api/IndexController.java
+++ src/main/java/com/medsys/medsysapi/api/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "https://apimedsys.azurewebsites.net")
 @RestController
 public class IndexController {
     @GetMapping({"/", "/index"})
