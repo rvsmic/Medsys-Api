cb034d9cfbc4350e63f3aedf4d56b603a4fbbf9e
adampolanski <adam.polanski02@gmail.com>
2024-05-29 11:21:14 +0200

    Updated error handling for codes: 400, 404, 405

--- src/main/java/com/medsys/medsysapi/utils/ErrorResponseHandler.java
+++ src/main/java/com/medsys/medsysapi/utils/ErrorResponseHandler.java
@@ -2,10 +2,13 @@ package com.medsys.medsysapi.utils;
 
 import com.medsys.medsysapi.db.QueryException;
 import com.nimbusds.oauth2.sdk.http.HTTPResponse;
+import org.apache.coyote.BadRequestException;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,6 +25,24 @@ public class ErrorResponseHandler {
             return generateErrorResponse(HTTPResponse.SC_SERVER_ERROR, e);
         }
         return new BasicResponse(e.getStatus(), e.getMessage()).generateResponse();
+    }
+
+    @Order(1)
+    @ExceptionHandler({BadRequestException.class})
+    public ResponseEntity badRequestHandler(BadRequestException e) {
+        return generateErrorResponse(HTTPResponse.SC_BAD_REQUEST, e);
+    }
+
+    @Order(1)
+    @ExceptionHandler({NoResourceFoundException.class})
+    public ResponseEntity notFoundHandler(NoResourceFoundException e) {
+        return generateErrorResponse(HTTPResponse.SC_NOT_FOUND, e);
+    }
+
+    @Order(1)
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
+    public ResponseEntity methodNotAllowedHandler(HttpRequestMethodNotSupportedException e) {
+        return generateErrorResponse(405, e);
     }
 
     @Order(2)
