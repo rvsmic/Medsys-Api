f68bf535cfc10c47fa464c0d631ec5365977f659
adampolanski <adam.polanski02@gmail.com>
2024-06-01 16:29:05 +0200

    Fixed class cast exception not handled in JsonHandler

--- src/main/java/com/medsys/medsysapi/utils/JsonHandler.java
+++ src/main/java/com/medsys/medsysapi/utils/JsonHandler.java
@@ -3,6 +3,7 @@ package com.medsys.medsysapi.utils;
 import net.minidev.json.JSONObject;
 import net.minidev.json.JSONValue;
 import net.minidev.json.parser.ParseException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 
 public class JsonHandler {
 
@@ -11,9 +12,12 @@ public class JsonHandler {
     }
 
     public static JSONObject readJsonData(String data) throws ParseException {
-        JSONObject jsonData = new JSONObject();
-        jsonData = (JSONObject) JSONValue.parseWithException(data);
-
-        return jsonData;
+        try {
+            JSONObject jsonData = new JSONObject();
+            jsonData = (JSONObject) JSONValue.parseWithException(data);
+            return jsonData;
+        } catch (ClassCastException e) {
+            throw new HttpMessageNotReadableException("Could not read JSON data.", e);
+        }
     }
 }
